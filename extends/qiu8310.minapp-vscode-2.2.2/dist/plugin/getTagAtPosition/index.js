f8607a34b1162f530952719a91b21618c16dd5a9
longzi <891661163@qq.com>
2020-11-08 22:51:32 +0800

    精简功能 只保留跳转功能

--- extends/qiu8310.minapp-vscode-2.2.2/dist/plugin/getTagAtPosition/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getVueTag_1 = require("./getVueTag");
-exports.getTagAtPosition = getVueTag_1.getVueTag;
-//# sourceMappingURL=index.js.map
